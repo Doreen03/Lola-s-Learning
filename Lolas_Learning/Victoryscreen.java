84146b4ee63aba23acc90b25112da9118798fc20
EmFlo14 <89649507+EmFlo14@users.noreply.github.com>
2021-12-13 19:31:02 -0500

    Worked on the game over screen

--- Lolas_Learning/Victoryscreen.java
+++ Lolas_Learning/Victoryscreen.java
@@ -16,6 +16,7 @@ public class Victoryscreen extends World
     public Victoryscreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1152, 648, 1); 
+        super(1152, 648, 1);
     }
+    
 }
