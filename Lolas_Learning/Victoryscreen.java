e7100063157228801b43844e1cd647f17d54cc7b
EmFlo14 <89649507+EmFlo14@users.noreply.github.com>
2021-12-13 21:33:52 -0500

    Added victory screen

--- Lolas_Learning/Victoryscreen.java
+++ Lolas_Learning/Victoryscreen.java
@@ -17,6 +17,20 @@ public class Victoryscreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1152, 648, 1);
+        prepare();
     }
-    
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        VictoryBook victoryBook = new VictoryBook();
+        addObject(victoryBook,580,275);
+        VictoryText victoryText = new VictoryText();
+        addObject(victoryText,573,318);
+        GameOverRestart gameOverRestart = new GameOverRestart();
+        addObject(gameOverRestart,105,600);
+    }
 }
